e298f8e7001b7d79f5e1a5b649808c63c07e2924
Ekaterina Gil <Ekaterina.Gil@gmail.com>
2023-11-19 02:03:17 +0300

    lesson_6

---
+++ lesson_6/2.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(
+    ChromeDriverManager().install()))
+driver.implicitly_wait(5)
+
+driver.get("http://uitestingplayground.com/textinput")
+driver.find_element(By.CSS_SELECTOR, '#newButtonName').send_keys('SkyPro')
+button = driver.find_element(By.CSS_SELECTOR, "#updatingButton")
+button.click()
+text_content = button.text
+print(text_content)
